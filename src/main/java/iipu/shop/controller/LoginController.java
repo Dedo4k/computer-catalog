2f8ebff035a5d3f58ef9eaae3e0ce144ae42c5bf
Dedo4k <lailo.vlad@gmail.com>
2021-11-01 01:59:08 +0300

    views, bootstrap, logout

--- src/main/java/iipu/shop/controller/LoginController.java
+++ src/main/java/iipu/shop/controller/LoginController.java
@@ -1,7 +1,13 @@
 package iipu.shop.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -9,5 +15,14 @@ public class LoginController {
     @GetMapping("/sign-in")
     public String signIn() {
         return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
     }
 }
--- src/main/java/iipu/shop/controller/MainPageController.java
+++ src/main/java/iipu/shop/controller/MainPageController.java
@@ -3,13 +3,15 @@ package iipu.shop.controller;
 import iipu.shop.model.User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainPageController {
 
     @GetMapping({"/", "/main"})
-    public String main(@AuthenticationPrincipal User user) {
+    public String main(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("user", user);
         return "main";
     }
 }
