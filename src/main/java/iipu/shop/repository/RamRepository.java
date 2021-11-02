c428c4ef8171bb24e2f163576b22eb10e3f56812
alexey1902 <alesha_1902@mail.ru>
2021-11-02 23:08:13 +0300

    ram-added-lesha

---
+++ src/main/java/iipu/shop/repository/RamRepository.java
@@ -0,0 +1,9 @@
+package iipu.shop.repository;
+
+import iipu.shop.model.component.Ram;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RamRepository extends JpaRepository<Ram, Long> {
+}
