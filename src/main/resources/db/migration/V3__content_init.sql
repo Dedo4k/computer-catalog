0afb973d9cb756bad8cbf7dababf7dd71f66f5c6
Ivan Klishevskiy <i.klishevskiy@mail.ru>
2021-11-14 16:12:33 +0300

    add 68 source.

--- src/main/resources/db/migration/V3__content_init.sql
+++ src/main/resources/db/migration/V3__content_init.sql
@@ -20,110 +20,302 @@ insert into component (id, model, producer, price)
 insert into processor (core, max_freq, min_freq, socket, id)
        values ('Ryzen 5', 4.2, 3.6, 'AM4', 2);
 
+insert into component_source (id, component_id, name, link)
+values (5, 2, 'amd.by', 'https://www.amd.by/processory/amd-ryzen-5-3600/');
+
+insert into component_source (id, component_id, name, link)
+values (6, 2, '234.by', 'https://234.by/amd-ryzen-5-3600-2.html?utm_source=shop_by');
+
+insert into component_source (id, component_id, name, link)
+values (7, 2, 'ram.by', 'https://ram.by/amd-ryzen-5-3600.html?utm_source=shop_by_yml&utm_medium=place&utm_campaign=8&utm_term=443039');
+
+insert into component_source (id, component_id, name, link)
+values (8, 2, 'fk.by', 'https://fk.by/processor-amd-ryzen-5-3600-multipack-145437');
+
 insert into component (id, model, producer, price)
        values (3, '11400', 'Intel', 555.00);
 insert into processor (core, max_freq, min_freq, socket, id)
        values ('Core i5', 4.4, 2.6, 'LGA1200', 3);
+
+insert into component_source (id, component_id, name, link)
+values (9, 3, 'amd.by', 'https://www.amd.by/processory/intel-core-i5-11400/');
+
+insert into component_source (id, component_id, name, link)
+values (10, 3, '234.by', 'https://234.by/processor-intel-core-i5-11400.html?utm_source=shop_by');
+
+insert into component_source (id, component_id, name, link)
+values (11, 3, 'shop.by', 'https://shop.by/protsessory/intel_core_i5_11400_box/#shop');
 
 insert into component (id, model, producer, price)
        values (4, '3700X', 'AMD', 742.19);
 insert into processor (core, max_freq, min_freq, socket, id)
        values ('Ryzen 7', 4.4, 3.6, 'AM4', 4);
 
+insert into component_source (id, component_id, name, link)
+values (12, 4, 'amd.by', 'https://www.amd.by/processory/amd-ryzen-7-3700x/');
+
+insert into component_source (id, component_id, name, link)
+values (13, 4, '234.by', 'https://234.by/amd-ryzen-7-3700x-2.html?utm_source=shop_by');
+
+insert into component_source (id, component_id, name, link)
+values (14, 4, 'shop.by', 'https://shop.by/protsessory/amd_ryzen_7_3700x/#shop');
+
 insert into component (id, model, producer, price)
        values (5, '10700K', 'Intel', 923.47);
 insert into processor (core, max_freq, min_freq, socket, id)
        values ('Core i7', 5.1, 3.8, 'LGA1200', 5);
+
+insert into component_source (id, component_id, name, link)
+values (15, 5, 'shop.by', 'https://shop.by/protsessory/intel_core_i7_10700k/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (16, 5, 'amd.by', 'https://www.amd.by/processory/intel-core-i7-10700k/');
+
+insert into component_source (id, component_id, name, link)
+values (17, 5, '234.by', 'https://234.by/intel-core-i7-10700k-box.html?utm_source=shop_by');
 
 insert into component (id, model, producer, price)
        values (6, 'DUAL', 'Palit', 2399.00);
 insert into graphics_card (gpu_model, gpu_producer, video_memory, video_memory_type, id)
        values ('GeForce RTX 3060', 'NVIDEA', 12, 'GDDR6', 6);
 
+insert into component_source (id, component_id, name, link)
+values (18, 6, 'shop.by', 'https://shop.by/videokarty/palit_geforce_rtx_3060_dual_12gb_gddr6_ne63060019k9_190ad/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (19, 6, 'amd.by', 'https://www.amd.by/videokarty/palit-geforce-rtx-3060-dual-12gb-gddr6-ne63060019k9-190ad/');
+
+insert into component_source (id, component_id, name, link)
+values (20, 6, '234.by', 'https://234.by/palit-geforce-rtx-3060-dual-12gb-gddr6-ne63060019k9-190ad.html?utm_source=shop_by');
+
+insert into component_source (id, component_id, name, link)
+values (21, 6, 'ram.by', 'https://ram.by/videokarta-palit-geforce-rtx-3060-dual-12gb-gddr6-ne63060019k9-190ad.html?utm_source=shop_by_yml&utm_medium=place&utm_campaign=15&utm_term=470114');
+
+insert into component_source (id, component_id, name, link)
+values (22, 6, 'fk.by', 'https://fk.by/videokarta-palit-geforce-rtx-3060-dual-12gb-gddr6-ne63060019k9-190ad-156051');
+
 insert into component (id, model, producer, price)
        values (7, 'D6 WINDFORCE OC', 'Gigabyte', 1030.19);
 insert into graphics_card (gpu_model, gpu_producer, video_memory, video_memory_type, id)
        values ('GeForce GTX 1650', 'NVIDEA', 4, 'GDDR6', 7);
+
+insert into component_source (id, component_id, name, link)
+values (23, 7, 'shop.by', 'https://shop.by/videokarty/gigabyte_gv_n1650oc_4gd/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (24, 7, 'ram.by', 'https://ram.by/videokarta-gigabyte-nvidia-geforce-gtx-1650-4gb-gv-n1650oc-4gd.html?utm_source=shop_by_yml&utm_medium=place&utm_campaign=15&utm_term=437171');
+
+insert into component_source (id, component_id, name, link)
+values (25, 7, 'fk.by', 'https://fk.by/videokarta-gigabyte-geforce-gtx-1650-oc-4gb-gddr5-gv-n1650oc-4gd-115102');
 
 insert into component (id, model, producer, price)
        values (8, 'V2 OC Edition', 'ASUS ROG Strix', 2899.00);
 insert into graphics_card (gpu_model, gpu_producer, video_memory, video_memory_type, id)
        values ('GeForce RTX 3060 Ti', 'NVIDEA', 8, 'GDDR6', 8);
 
+insert into component_source (id, component_id, name, link)
+values (26, 8, 'shop.by', 'https://shop.by/videokarty/asus_rog_strix_geforce_rtx_3060_ti_v2_oc_edition_8gb_gddr6_lhr/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (27, 8, 'izliv.by', 'https://izliv.by/asus_rog_strix_geforce_rtx_3060_ti_v2_oc_edition_8gb_gddr6_lhr');
+
 insert into component (id, model, producer, price)
        values (9, 'Ventus XS OC', 'MSI', 2050.00);
 insert into graphics_card (gpu_model, gpu_producer, video_memory, video_memory_type, id)
        values ('GeForce GTX 1660 Super', 'NVIDEA', 6, 'GDDR6', 9);
+
+insert into component_source (id, component_id, name, link)
+values (28, 9, 'shop.by', 'https://shop.by/videokarty/msi_gtx_1660_super_ventus_xs_oc/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (29, 9, 'ram.by', 'https://ram.by/videokarta-msi-geforce-gtx-1660-super-ventus-xs-oc-6gb-gddr5.html?utm_source=shop_by_yml&utm_medium=place&utm_campaign=15&utm_term=446610');
+
+insert into component_source (id, component_id, name, link)
+values (30, 9, 'fk.by', 'https://fk.by/videokarta-msi-geforce-gtx-1660-super-ventus-xs-oc-6gb-gddr6-124784');
 
 insert into component (id, model, producer, price)
        values (10, 'TUF Gaming OC Edition', 'ASUS', 5990.00);
 insert into graphics_card (gpu_model, gpu_producer, video_memory, video_memory_type, id)
        values ('GeForce RTX 3080 Ti', 'NVIDEA', 12, 'GDDR6X', 10);
 
+insert into component_source (id, component_id, name, link)
+values (31, 10, 'shop.by', 'https://shop.by/videokarty/asus_tuf_gaming_geforce_rtx_3080_ti_12gb_gddr6x/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (32, 10, 'amd.by', 'https://www.amd.by/videokarty/asus-tuf-gaming-geforce-rtx-3080-ti-12gb-gddr6x/');
+
+insert into component_source (id, component_id, name, link)
+values (33, 10, 'fk.by', 'https://fk.by/videokarta-asus-tuf-gaming-geforce-rtx-3080-ti-12gb-gddr6x-164351');
+
 insert into component (id, model, producer, price)
        values (11, 'B450M S2H', 'Gigabyte', 166.02);
 insert into mother_board (pci_version, form_factor, m_two, memory_slots, memory_type, socket, id)
        values ('3.0 and 2.0', 'mAtx', 1, 2, 'DDR4', 'AM4', 11);
+
+insert into component_source (id, component_id, name, link)
+values (34, 11, 'shop.by', 'https://shop.by/materinskie_platy/gigabyte_b450m_s2h_rev_1_0/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (35, 11, 'amd.by', 'https://www.amd.by/materinskie-platy/gigabyte-b450m-s2h-v2-rev--1-0/');
+
+insert into component_source (id, component_id, name, link)
+values (36, 11, '234.by', 'https://234.by/materinskaja-plata-gigabyte-b450m-s2h-v2-rev-1-0.html?utm_source=shop_by');
 
 insert into component (id, model, producer, price)
        values (12, 'B560M-HDV', 'ASRock', 205.07);
 insert into mother_board (pci_version, form_factor, m_two, memory_slots, memory_type, socket, id)
        values ('4.0 and 3.0', 'mAtx', 2, 2, 'DDR4', 'LGA1200', 12);
 
+insert into component_source (id, component_id, name, link)
+values (37, 12, 'shop.by', 'https://shop.by/materinskie_platy/asrock_b560m_hdv/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (38, 12, 'amd.by', 'https://www.amd.by/materinskie-platy/asrock-b560m-hdv/');
+
+insert into component_source (id, component_id, name, link)
+values (39, 12, '234.by', 'https://234.by/asrock-b560m-hdv.html?utm_source=shop_by');
+
 insert into component (id, model, producer, price)
        values (13, 'TUF B450M-Pro Gaming', 'ASUS', 237.39);
 insert into mother_board (pci_version, form_factor, m_two, memory_slots, memory_type, socket, id)
        values ('3.0 and 2.0', 'mAtx', 2, 4, 'DDR4', 'AM4', 13);
+
+insert into component_source (id, component_id, name, link)
+values (40, 13, 'shop.by', 'https://shop.by/materinskie_platy/asus_tuf_b450m_pro_gaming/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (41, 13, 'amd.by', 'https://www.amd.by/materinskie-platy/asus-tuf-b450m-pro-gaming/');
+
+insert into component_source (id, component_id, name, link)
+values (42, 13, '234.by', 'https://234.by/materinskaja-plata-asus-tuf-b450m-pro-gaming-1.html?utm_source=shop_by');
 
 insert into component (id, model, producer, price)
        values (14, 'MAG B550 Tomahawk', 'MSI', 467.00);
 insert into mother_board (pci_version, form_factor, m_two, memory_slots, memory_type, socket, id)
        values ('4.0 and 3.0', 'ATX', 2, 4, 'DDR4', 'AM4', 14);
 
+insert into component_source (id, component_id, name, link)
+values (43, 14, 'shop.by', 'https://shop.by/materinskie_platy/msi_mag_b550_tomahawk/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (44, 14, 'force.by', 'https://www.force.shop.by/kompjuternaja-tekhnika/kompjuternye-komplektujuschie/materinskie-platy/materinskaja-plata-msi-mag-b550-tomahawk/');
+
 insert into component (id, model, producer, price)
        values (15, 'Z690 Gaming X', 'Gigabyte', 763.15);
 insert into mother_board (pci_version, form_factor, m_two, memory_slots, memory_type, socket, id)
        values ('5.0', 'ATX', 4, 4, 'DDR4', 'LGA1700', 15);
+
+insert into component_source (id, component_id, name, link)
+values (45, 15, 'fk.by', 'https://fk.by/materinskaya-plata-gigabyte-z690-gaming-x-ddr4-rev-1-0-167557');
 
 insert into component (id, model, producer, price)
        values (16, 'Pure Base 500 BG035', 'be quiet!', 195.03);
 insert into computer_case (case_type, colour, material, section_number, id)
        values ('Mid Tower', 'white/белый', 'metal/метал', 6, 16);
 
+insert into component_source (id, component_id, name, link)
+values (46, 16, 'shop.by', 'https://shop.by/korpusa_dlya_kompyutera/be_quiet_pure_base_500_bg035/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (47, 16, 'ram.by', 'https://ram.by/korpus-be-quiet-pure-base-500-bg035.html?utm_source=shop_by_yml&utm_medium=place&utm_campaign=53&utm_term=448226');
+
+insert into component_source (id, component_id, name, link)
+values (48, 16, 'fk.by', 'https://fk.by/korpus-be-quiet-pure-base-500-bg035-136477');
+
 insert into component (id, model, producer, price)
        values (17, 'MATREXX 55 V3', 'DeepCool', 186.03);
 insert into computer_case (case_type, colour, material, section_number, id)
        values ('Mid Tower', 'white/белый', 'metal/метал', 6, 17);
+
+insert into component_source (id, component_id, name, link)
+values (49, 17, 'shop.by', 'hhttps://shop.by/korpusa_dlya_kompyutera/deepcool_matrexx_55_v3_add_rgb_3f_dp_atx_matrexx55v3_ar_3f/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (50, 17, 'itmarket.by', 'https://itmarket.by/komplektuyuschie-i-aksessuary/Komplektuyuschie/korpusa/korpus-deepcool-matrexx-55-v3-dp-atx-matrexx55v3-ar-3f/');
+
+insert into component_source (id, component_id, name, link)
+values (51, 17, 'ram.by', 'https://ram.by/korpus-deepcool-matrexx-55-v3-add-rgb-dp-atx-matrexx55v3-ar-3f.html?utm_source=shop_by_yml&utm_medium=place&utm_campaign=53&utm_term=449004');
 
 insert into component (id, model, producer, price)
        values (18, 'Silent Base 802 Window', 'be quiet!', 474.60);
 insert into computer_case (case_type, colour, material, section_number, id)
        values ('Mid Tower', 'black/черный', 'steel/сталь', 6, 18);
 
+insert into component_source (id, component_id, name, link)
+values (52, 18, 'shop.by', 'https://shop.by/korpusa_dlya_kompyutera/be_quiet_silent_base_802_bgw39/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (53, 18, 'itmarket.by', 'https://itmarket.by/komplektuyuschie-i-aksessuary/Komplektuyuschie/korpusa/korpus-bgw39-be-quiet-silent-base-802-window-black-eatx-usb-3-2-type-c-3xfan/');
+
+insert into component_source (id, component_id, name, link)
+values (54, 18, 'socket.by', 'https://www.socket.by/kompyutery/komplektuyushchie-dlya-pk/kompyuternye-korpusa-bez-bloka-pitaniya/korpus-be-quiet-silent-base-802-bgw39-chyernyy-s-oknom-bez-bp/');
+
 insert into component (id, model, producer, price)
        values (19, 'N5 MF', 'Zalman', 100.20);
 insert into computer_case (case_type, colour, material, section_number, id)
        values ('Mid Tower', 'black/черный', 'plastic/пластик', 5, 19);
+
+insert into component_source (id, component_id, name, link)
+values (55, 19, 'shop.by', 'https://shop.by/korpusa_dlya_kompyutera/zalman_n5_mf/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (56, 19, 'ram.by', 'https://ram.by/korpus-zalman-n5-mf.html?utm_source=shop_by_yml&utm_medium=place&utm_campaign=53&utm_term=457757');
+
+insert into component_source (id, component_id, name, link)
+values (57, 19, 'fk.by', 'https://fk.by/korpus-zalman-n5-mf-142478');
 
 insert into component (id, model, producer, price)
        values (20, 'FireBase X2', '1stPlayer', 94.99);
 insert into computer_case (case_type, colour, material, section_number, id)
        values ('Mid Tower', 'black/черный', 'steel/сталь', 7, 20);
 
+insert into component_source (id, component_id, name, link)
+values (58, 20, 'fk.by', 'https://fk.by/korpus-1stplayer-firebase-x2-128190');
+
+insert into component_source (id, component_id, name, link)
+values (59, 20, 'multicom.by', 'https://multicom.by/catalog/korpusa/1stplayer-firebase-x2?utm_source=market&utm_term=M0230628');
+
 insert into component (id, model, producer, price)
        values (21, 'P2', 'Crucial', 245.84);
 insert into ssd (capacity, controller, form_factor, reading_speed, ssd_interface, writing_speed, id)
        values (1000, 'Phison PS5013-E13T', 'M.2', 2400, 'PCI Express 3.0 x4  (NVMe)', 1800, 21);
+
+insert into component_source (id, component_id, name, link)
+values (60, 21, 'shop.by', 'https://shop.by/zhestkie_diski/crucial_p2_ct1000p2ssd8/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (61, 21, 'amd.by', 'https://www.amd.by/344-ssd/crucial-p2-1tb-ct1000p2ssd8/');
+
+insert into component_source (id, component_id, name, link)
+values (62, 21, '234.by', 'https://234.by/crucial-p2-1tb-ct1000p2ssd8.html?utm_source=shop_by');
 
 insert into component (id, model, producer, price)
        values (22, '870 Evo', 'Samsung', 224.85);
 insert into ssd (capacity, controller, form_factor, reading_speed, ssd_interface, writing_speed, id)
        values (500, 'Samsung MKX', '2.5"', 560, 'SATA 3.0', 530, 22);
 
+insert into component_source (id, component_id, name, link)
+values (63, 22, 'shop.by', 'https://shop.by/zhestkie_diski/samsung_870_evo_1tb_mz_77e1t0bw/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (64, 22, 'amd.by', 'https://www.amd.by/344-ssd/samsung-870-evo-1tb-mz-77e1t0bw/');
+
+insert into component_source (id, component_id, name, link)
+values (65, 22, '234.by', 'https://234.by/samsung-870-evo-1tb-mz-77e1t0bw.html?utm_source=shop_by');
+
 insert into component (id, model, producer, price)
        values (23, 'NV1', 'Kingston', 141.80);
 insert into ssd (capacity, controller, form_factor, reading_speed, ssd_interface, writing_speed, id)
        values (500, '-', 'M.2', 2000, 'PCI Express 3.0 x4', 1700, 23);
+
+insert into component_source (id, component_id, name, link)
+values (66, 23, 'shop.by', 'https://shop.by/zhestkie_diski/kingston_nv1_2tb_snvs_2000g/#shop');
+
+insert into component_source (id, component_id, name, link)
+values (67, 23, 'amd.by', 'https://www.amd.by/344-ssd/kingston-nv1-2tb-snvs2000g/');
+
+insert into component_source (id, component_id, name, link)
+values (68, 23, '234.by', 'https://234.by/ssd-kingston-nv1-2tb-snvs-2000g.html?utm_source=shop_by');
 
 insert into component (id, model, producer, price)
        values (24, 'Radeon R5', 'AMD', 142.04);
